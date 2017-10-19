e54b44d0a132cde4b5a5adb3cba2434beb08ae7c
Slava <vgolovko2017@gmail.com>
2017-10-19 14:44:33 +0300

    added date check

--- getcdate.php
+++ getcdate.php
@@ -3,10 +3,17 @@
 		
 	if (!isset($_GET['date'])) {
 		echo "Error: wrong parameters";
-		return false;
+		return true;
 	}
 		
 	$date = $_GET['date'];
+	
+	list($yyyy, $mm, $dd) = explode("-", $date);	
+	if (!checkdate($mm, $dd, $yyyy)) {
+		echo "Error: Incorrect date format";
+		return false;
+	}
+	
 	$dbInfo = new GetInfoFromDb();
 
 	return $dbInfo->getExchangeByDate($date);
