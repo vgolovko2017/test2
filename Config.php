6960c7e644a59bbc2fb8d421d19bb7a3de100976
Slava <vgolovko2017@gmail.com>
2017-10-19 12:06:54 +0300

    Cosmetick changes

--- Config.php
+++ Config.php
@@ -1,8 +1,8 @@
 <?php
-		const USERNAME="root";
-		const PASSWORD="";
-		const HOST="localhost";
-		const DB="main_db";
-		const URL = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=EUR&date=";
-		const CC = "EUR";
-		const PATH_TO_LOG_FILE = "C:\Log\cron_script.log";
+	const USERNAME="root";
+	const PASSWORD="";
+	const HOST="localhost";
+	const DB="main_db";
+	const URL = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=EUR&date=";
+	const CC = "EUR";
+	const PATH_TO_LOG_FILE = "C:\Log\cron_script.log";
