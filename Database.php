c6c5f4f59dfe05c65392ca76b3c5b8744e0517ba
vgolovko <vgolovko2017@gmail.com>
2020-02-12 16:42:38 +0200

    remove - new fix on dev

--- Database.php
+++ Database.php
@@ -10,7 +10,7 @@
 		private $db = DB;
 
 		/*
-		Get an instance of the Database new fix on dev
+		Get an instance of the Database
 		@return Instance
 		*/
 		public static function getInstance() {
